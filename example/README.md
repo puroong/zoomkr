5186a5d61485c428f89babc565f3362b2f5156ba
오범수 <skfk0135@gmail.com>
2021-05-18 18:24:50 +0900

    chore: add example

---
+++ example/README.md
@@ -0,0 +1,5 @@
+# Example
+
+[README.md 링크](https://github.com/zoom/zoom-oauth-sample-app/blob/master/README.md)
+
+예시는 [zoom-oauth-sample-app](https://github.com/zoom/zoom-oauth-sample-app)을 참고하여 작성하였습니다.
---
+++ example/index.js
@@ -0,0 +1,28 @@
+require('dotenv/config')
+
+const express = require('express')
+const zoom = require('zoomkr');
+const app = express()
+
+app.get('/', async (req, res) => {
+    if(req.query.code) {
+        const credential = {
+            clientId: process.env.clientID,
+            clientSecret: process.env.clientSecret,
+        }
+
+        const parameter = {
+            grantType: 'authorization_code',
+            code: req.query.code,
+            redirectUri: process.env.redirectURL
+        }
+
+        const accessToken = await zoom.auth.getAccessToken(credential, parameter);
+        const newMeeting = await zoom.meeting.create({ param: { userId: 'me' }, body: {}, accessToken });
+        const meetings = await zoom.meeting.list({ param: { userId: 'me' }, query: {}, accessToken });
+        
+        res.status(200).json(meetings);
+    }
+})
+
+app.listen(4000, () => console.log(`Zoom Hello World app listening at PORT: 4000`))
