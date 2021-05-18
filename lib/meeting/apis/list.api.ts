049208a603fa60dc2e1f9d96543f70b1505bcca1
오범수 <skfk0135@gmail.com>
2021-05-18 15:46:42 +0900

    refactor: 인터페이스 & 타입들 전부 api 파일로 이동

--- lib/meeting/apis/list.api.ts
+++ lib/meeting/apis/list.api.ts
@@ -22,3 +22,31 @@ export async function listMeeting(payload: ListMeetingPayload) {
     throw err;
   }
 }
+
+export interface ListMeetingParam {
+  userId: string;
+}
+
+enum MeetingType {
+  SCHEDULED = 'scheduled',
+  LIVE = 'live',
+  UPCOMING = 'upcoming',
+}
+
+export interface ListMeetingQuery {
+  type?: MeetingType;
+
+  page_size?: number;
+
+  next_page_token?: string;
+
+  page_number?: string;
+}
+
+export interface ListMeetingPayload {
+  param: ListMeetingParam;
+
+  query: ListMeetingQuery;
+
+  accessToken: string;
+}
