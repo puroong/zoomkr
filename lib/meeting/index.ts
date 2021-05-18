389b3768a8eabc29a899b574fc4a2992d94e7e70
오범수 <skfk0135@gmail.com>
2021-05-18 16:29:02 +0900

    feat: delete meeting

--- lib/meeting/index.ts
+++ lib/meeting/index.ts
@@ -1,7 +1,9 @@
 import { createMeeting } from './apis/create.api';
+import { deleteMeeting } from './apis/delete';
 import { listMeeting } from './apis/list.api';
 
 export const meeting = {
   create: createMeeting,
   list: listMeeting,
+  delete: deleteMeeting,
 };
