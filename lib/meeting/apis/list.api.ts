f24006a241e908a09f385d098189bb78b271b797
오범수 <skfk0135@gmail.com>
2021-05-18 15:47:17 +0900

    refactor: 필요없는 import문 제거

--- lib/meeting/apis/list.api.ts
+++ lib/meeting/apis/list.api.ts
@@ -1,6 +1,5 @@
 import axios from '../../core/axios';
 import { stringify } from '../../core/helpers/querstring.helper';
-import { ListMeetingParam, ListMeetingPayload } from '../types';
 
 function listMeetingPath(payload: ListMeetingParam) {
   return `/users/${payload.userId}/meetings`;
