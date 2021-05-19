5aefbf1368123358cfed78c80383479829dd3ba1
오범수 <skfk0135@gmail.com>
2021-05-19 16:00:36 +0900

    feat: get meeting

---
+++ lib/meeting/apis/get-one.ts
@@ -0,0 +1,41 @@
+import axios from '../../common/axios';
+import { stringify } from '../../common/helpers/querstring.helper';
+
+function getMeetingPath(payload: GetMeetingParam) {
+  return `/meetings/${payload.meetingId}`;
+}
+
+export async function getMeeting(payload: GetMeetingPayload) {
+  const { param, query, accessToken } = payload;
+  const path = `${getMeetingPath(param)}?${stringify(query)}`;
+
+  try {
+    const { data } = await axios.get(path, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export interface GetMeetingParam {
+  meetingId: number;
+}
+
+export interface GetMeetingQuery {
+  occurrence_id: string;
+
+  show_previous_occurences: boolean;
+}
+
+export interface GetMeetingPayload {
+  param: GetMeetingParam;
+
+  query: GetMeetingQuery;
+
+  accessToken: string;
+}
--- lib/meeting/index.ts
+++ lib/meeting/index.ts
@@ -1,9 +1,11 @@
 import { createMeeting } from './apis/create';
 import { deleteMeeting } from './apis/delete';
+import { getMeeting } from './apis/get-one';
 import { listMeeting } from './apis/list';
 
 export const meeting = {
   create: createMeeting,
   list: listMeeting,
   delete: deleteMeeting,
+  get: getMeeting,
 };
